a2ed20bd83483549432302817f1d5aec31087b0c
Selihei <selina.heinrichlp@gmail.com>
2025-06-11 14:04:41 +0200

    rain calculation

--- main.dart
+++ main.dart
@@ -6,16 +6,25 @@ List<Map<String, double?>> weatherData = [
 {'temp': null, 'rain': 3.8, 'wind': null}
 ];
 
-// Erstelle leere Liste für Temperaturen
+// Erstelle leere Listen für die Werte
 List<double> temps = [];
+List<double> rains = [];
+List<double> winds = [];
 
-//Füge die Temperaturen der Wetterdaten in die Liste ein
+//Füge die Werte der Wetterdaten in die Listen ein
 temps.add(weatherData[0]['temp'] ?? 0);
 temps.add(weatherData[1]['temp'] ?? 0);
 temps.add(weatherData[2]['temp'] ?? 0);
 
-// Berechne den Durchschnitt der Temperaturen
+rains.add(weatherData[0]['rain'] ?? 0);
+rains.add(weatherData[1]['rain'] ?? 0);
+rains.add(weatherData[2]['rain'] ?? 0);
+
+
+// Berechnnug der Durchschnitte
 double avgTemp = (temps[0] + temps[1] + temps[2]) / temps.length;
+double avgRain = (rains[0] + rains[1] + rains[2]) / rains.length;
 print('Durchschnittstemperatur: $avgTemp °C');
+print('Durchschnitt Niederschlagsmenge: $avgRain mm');
 }
 
